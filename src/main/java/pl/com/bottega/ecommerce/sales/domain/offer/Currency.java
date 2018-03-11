ac3a4b152a7130afb1086e06798f33b6873f1927
U-DESKTOP-IB7GL2T\Parada <paradowskim94@gmail.com>
2018-03-11 21:25:10 +0100

    added currency parameters and getters

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Currency.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Currency.java
@@ -2,4 +2,15 @@ package pl.com.bottega.ecommerce.sales.domain.offer;
 
 public class Currency {
 
+    private String currency;
+    private String currencyCode;
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public String getCurrencyCode() {
+
+        return currencyCode;
+    }
 }
